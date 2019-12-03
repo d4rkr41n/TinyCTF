3a3c51ea53b91b78f124cc4763d3b60f6c4dcde8
d4rkr41n <you@example.com>
2019-12-03 09:03:32 -0600

    Fixed Flag Counting Script

--- numFlagsGot.sh
+++ numFlagsGot.sh
@@ -1,1 +1,12 @@
-cat correctlog.txt | grep $(cat config.json | grep \"name\" | cut -d '"' -f 4 | awk '{print $1}' | sed "s/^/\(/" | sed "s/$/\)/") | grep -v "Start" | awk '{print $2$3$4}' | sort -u | wc -l
+readarray -t <<< $(cat config.json | grep \"name\" | cut -d '"' -f 4 | awk '{print $1}' | sed "s/^/\(/" | sed "s/$/\)/") # split to array $names
+
+for (( i=0; i<${#MAPFILE[@]}; i++ ))
+do
+    echo "$i: ${MAPFILE[$i]}"
+done
+read -p "Pick a team: " tnum
+
+activeTeam="${MAPFILE[$tnum]}"
+
+#activeTeam=$(cat config.json | grep \"name\" | grep \"active\"\:\"1\" | cut -d '"' -f 4 | awk '{print $1}' | sed "s/^/\(/" | sed "s/$/\)/")
+echo $(cat correctlog.txt | grep "$activeTeam" | grep -v "Start" | awk '{print $2$3$4}' | sort -u | wc -l) flags found
